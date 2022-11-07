88ce2baacb5ae15349e949d277486baef363e010
Leonardo <leopesi@yahoo.com.br>
2022-11-07 10:30:46 -0300

    refactoring credentials of db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('pesi', 'postgres', '123456', {
+//Credenciais
+const DB_DATABASE = process.env.DB_DATABASE
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+
+const db = new Sequelize(DB_DATABASE, DB_USER, DB_PASS, {
     host: 'localhost',
     dialect: 'postgres',
 
